fbec0fd0048ecce60f919648f2832e527f1c4036
ma-za-kpe <makpalyy@gmail.com>
2023-11-27 16:07:58 +0000

    add the bid response list

--- src/pages/buyer/customer.js
+++ src/pages/buyer/customer.js
@@ -3,6 +3,9 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import RequestsList from './RequestsList';
 import BidOrderForm from './BidOrderForm';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import BidResponseCard from './BidResponseCard';
 
 // TODO: repetition, please remove
 const crops = [
@@ -49,6 +52,98 @@ const crops = [
   // Add more crop interests as needed
 ];
 
+const farmer = {
+    personalInformation: {
+      firstName: 'John',
+      lastName: 'Doe',
+      dateOfBirth: '1980-01-15',
+      gender: 'Male',
+      address: '123 Farmer Street, Farmville',
+      email: 'john.doe@example.com',
+      phone: '+1234567890',
+    },
+    farmInformation: {
+      farmName: 'Doe Family Farm',
+      farmLocation: 'Farmville, Country',
+      farmSize: '50 acres',
+      crops: [
+        {
+          cropName: 'Maize',
+          quantity: '10 tons',
+          qualityRequirements: 'Grade A',
+          deliveryLocation: 'Port of Nairobi, Kenya',
+          deliveryTimeframe: 'Within 4 weeks',
+          description: 'This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like.',
+          packagingPreferences: '50 kg bags',
+          certifications: ['Organic Certified'],
+          negotiationTerms: {
+            price: 'Negotiable',
+            paymentTerms: '50% advance, 50% upon delivery',
+          },
+          additionalRequirements: 'None',
+          dateAdded: '2023-02-15',
+        },
+        {
+          cropName: 'Coffee Beans',
+          quantity: '5,000 kg',
+          qualityRequirements: 'Arabica, Grade AA',
+          deliveryLocation: 'Port of Dar es Salaam, Tanzania',
+          deliveryTimeframe: 'Within 6 weeks',
+          description: 'This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like.',
+          packagingPreferences: 'Jute bags',
+          certifications: ['Fair Trade Certified'],
+          negotiationTerms: {
+            price: 'Negotiable',
+            paymentTerms: '30% advance, 70% LC',
+          },
+          additionalRequirements: 'Rainforest Alliance certification required',
+          dateAdded: '2023-01-01',
+        },
+        // Add more crop interests as needed
+      ],
+    },
+    financialInformation: {
+      annualIncome: '$50,000',
+      creditScore: 750, // An example field for credit score
+      outstandingLoans: [
+        {
+          loanType: 'Equipment Loan',
+          amount: '$10,000',
+          interestRate: '5%',
+          monthlyPayment: '$500',
+        },
+        // Add more outstanding loans as needed
+      ],
+    },
+  };
+
+// Sample bid responses for demonstration
+const bidResponses = [
+    {
+      bidInfo: {
+        title: 'Maize Bid',
+        subheader: 'Quantity: 10 tons',
+        description: 'This is a bid for Grade A maize with specific delivery requirements.',
+        more: 'Additional details about the bid.',
+      },
+      farmerInfo: farmer,
+      onReject: (bidInfo) => console.log(`Rejected bid: ${bidInfo.title}`),
+      onApprove: (bidInfo) => console.log(`Approved bid: ${bidInfo.title}`),
+    },
+    {
+      bidInfo: {
+        title: 'Coffee Beans Bid',
+        subheader: 'Quantity: 5,000 kg',
+        description: 'This is a bid for Grade AA Arabica coffee beans with specific delivery requirements.',
+        more: 'Additional details about the bid.',
+      },
+      farmerInfo: farmer,
+      onReject: (bidInfo) => console.log(`Rejected bid: ${bidInfo.title}`),
+      onApprove: (bidInfo) => console.log(`Approved bid: ${bidInfo.title}`),
+    },
+    // Add more bid responses as needed
+  ];
+
 export default function Customer() {
     const [open, setOpen] = useState(false);
   
@@ -81,16 +176,33 @@ export default function Customer() {
     };
   
     return (
-      <Box sx={{ flexGrow: 1 }}>
-        <Button variant="contained" onClick={handleClickOpen} sx={{ marginBottom: 2 }}>
-          Place Bid Order
-        </Button>
-  
-        {/* BidOrderForm component for the bid order form */}
-        <BidOrderForm open={open} handleClose={handleClose} handlePlaceBid={handlePlaceBid} />
-  
-        {/* RequestsList component for the list of requests */}
-        <RequestsList crops={crops} />
-      </Box>
-    );
+        <Grid container spacing={2}>
+            {/* Button Outside Columns */}
+          <Grid item xs={12} sx={{ textAlign: 'center' }}>
+            <Button variant="contained" onClick={handleClickOpen} sx={{ marginBottom: 2 }}>
+              Create Order
+            </Button>
+          </Grid>
+          {/* Left Column */}
+          <Grid item xs={12} md={6}>
+            <Typography variant="h6" gutterBottom align="center">
+              Recent Bids
+            </Typography>
+            {/* Add a list of BidResponseCards here */}
+            {bidResponses.map((bidResponse, index) => (
+              <BidResponseCard key={index} {...bidResponse} />
+            ))}
+          </Grid>
+    
+          {/* Right Column */}
+          <Grid item xs={12} md={6}>
+            <Box sx={{ p: 2 }}>
+              <RequestsList crops={crops} />
+            </Box>
+          </Grid>
+    
+          {/* BidOrderForm component for the bid order form */}
+          <BidOrderForm open={open} handleClose={handleClose} handlePlaceBid={handlePlaceBid} />
+        </Grid>
+      );
   }
